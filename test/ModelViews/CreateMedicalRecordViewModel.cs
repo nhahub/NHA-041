305908b442ed0027200173f3008597b112da67e0
ahmed hossam <ahmedhossamahmed22@gmail.com>
2025-12-11 00:06:04 +0200

    remove duplicates

--- test/ModelViews/CreateMedicalRecordViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace test.ModelViews
-{
-    public class CreateMedicalRecordViewModel
-    {
-        public int animalId { get; set; }
-        public string status { get; set; }
-        public string? injurys { get; set; }
-    }
-}
